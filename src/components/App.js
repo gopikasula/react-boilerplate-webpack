44d53fb623a058bff52fa77a7030f78f43b32ddc
gopikasula <gopikasula@gopikasulas-MacBook-Pro.local>
2020-04-22 01:02:18 +0530

    babel loaders react, es15 presets configured with webpack

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            Hello React, After long time 
+        </div>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+module.exports = {
+    entry: "./src/index.js",
+    output:{
+        path: path.resolve(__dirname, "dist"),
+        filename: "./js/bundle.js"
+    },
+    module: {
+        rules: [
+          { 
+            test: /\.js$/,
+            exclude: "/node_modules",
+            use:{
+                loader: 'babel-loader',   
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }  
+            }
+         }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './dist/index.html'
+        })
+    ]
+}
